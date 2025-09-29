ed9a045b983a5c454d6371818e86ff1f1db72da5
AndrUsM <oksud22@gmail.com>
2025-09-29 23:30:42 +0300

    remove old migrations update prisma package

--- src/database/migrations/20250816094615_20250816124612/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Bucket" ALTER COLUMN "isUpdated" DROP NOT NULL;
--- src/database/migrations/20250929202426_init/migration.sql
+++ src/database/migrations/20250929202426_init/migration.sql
@@ -1,5 +1,5 @@
 -- CreateEnum
-CREATE TYPE "public"."FileEntityType" AS ENUM ('IMAGE', 'VIDEO', 'FILE', 'AUDIO', 'MS_DOCUMENT', 'PDF_DOCUMENT', 'JSON_DOCUMENT', 'DOCUMENT', 'ARCHIVE', 'CODE');
+CREATE TYPE "public"."FileEntityType" AS ENUM ('IMAGE', 'VIDEO', 'FILE', 'AUDIO');
 
 -- CreateEnum
 CREATE TYPE "public"."Permissions" AS ENUM ('READ', 'WRITE', 'DELETE');
@@ -12,7 +12,7 @@ CREATE TABLE "public"."File" (
     "optimizedUrl" TEXT NOT NULL,
     "extension" TEXT NOT NULL,
     "entityType" "public"."FileEntityType" NOT NULL DEFAULT 'FILE',
-    "uploadedAt" TIMESTAMP(3) NOT NULL,
+    "uploadedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "bucketId" INTEGER NOT NULL,
 
     CONSTRAINT "File_pkey" PRIMARY KEY ("id")
@@ -23,7 +23,7 @@ CREATE TABLE "public"."App" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "permissions" "public"."Permissions"[] DEFAULT ARRAY['READ', 'WRITE']::"public"."Permissions"[],
-    "createdAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
     "bucketId" INTEGER NOT NULL,
 
     CONSTRAINT "App_pkey" PRIMARY KEY ("id")
@@ -34,16 +34,19 @@ CREATE TABLE "public"."Bucket" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "folder" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "isUpdated" BOOLEAN NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "description" TEXT,
+    "isUpdated" BOOLEAN DEFAULT false,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "Bucket_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "App_bucketId_key" ON "public"."App"("bucketId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Bucket_name_key" ON "public"."Bucket"("name");
 
 -- AddForeignKey
 ALTER TABLE "public"."File" ADD CONSTRAINT "File_bucketId_fkey" FOREIGN KEY ("bucketId") REFERENCES "public"."Bucket"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
